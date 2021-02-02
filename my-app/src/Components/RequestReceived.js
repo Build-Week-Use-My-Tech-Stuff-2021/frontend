bfd0aa770e6567358674631c9afd1440f843abe0
Steven <stevenpmcfarlane@gmail.com>
2021-02-01 17:11:57 -0700

    appreciation message

--- my-app/src/Components/RequestReceived.js
+++ my-app/src/Components/RequestReceived.js
@@ -2,6 +2,7 @@ export default function RequestReceived() {
   return (
     <div>
       <h1>Thank you, ${`firstName`}!</h1>
+      <h1>We appreciate you!</h1>
       <h3>
         Your request is being processed. Please check your email for more
         information.

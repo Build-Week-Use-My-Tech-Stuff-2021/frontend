43d230bcec5afbbf856827da449bcb1b8fe674ce
Steven <stevenpmcfarlane@gmail.com>
2021-02-04 11:57:31 -0700

    fixed forms for birthday

--- my-app/src/Components/RequestReceived.js
+++ my-app/src/Components/RequestReceived.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import styled from 'styled-components'
 
-const history = useHistory();
+
+export default function RequestReceived() {
+  const history = useHistory();
 
 const routeToHome = () => {
   history.push("/");
 };
-export default function RequestReceived() {
   return (
-    <div>
+    <StyledRequestReceivedForm>
       <h1>Thank you!</h1>
       <h3>
         Your request is being processed. Please check your email for more
@@ -17,6 +19,25 @@ export default function RequestReceived() {
       <button className="Processing page button" onClick={routeToHome}>
         Home
       </button>
-    </div>
+    </StyledRequestReceivedForm>
   );
 }
+
+const StyledRequestReceivedForm = styled.div`
+  color: ${(pr) => pr.theme.primaryColor};
+  /* font-weight: ${(pr) => pr.theme.fontWeight}; */
+  font-style: ${(pr) => pr.theme.fontStyle};
+  font-size: ${(pr) => pr.theme.fontSize};
+  padding: ${(pr) => pr.theme.padding};
+  //border: ${(pr) => pr.theme.border};
+  max-width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+
+  //background-color: ${(pr) => pr.theme.backgroundColor};
+  button {
+    color: darkslategray;
+  }
+`;
